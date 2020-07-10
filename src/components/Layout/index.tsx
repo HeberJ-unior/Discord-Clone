166b3fc483fa7ef8a62e4a7d6f199982af29f3be
Heber Junior <heberjr2001@gmail.com>
2020-07-10 15:21:15 -0300

    Atualização: 10/17/20

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// export default function Layout() {
+//     return(
+//         <div>Olá</div>
+//     )
+// }
+import { Grid } from './styles';
+
+const Layout: React.FC = ( ) => {
+    return (
+        <Grid>
+            
+
+        </Grid >
+    );
+};
+
+export default Layout;
